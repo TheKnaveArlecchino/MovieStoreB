46ba196863a62d96c9d03086833e28dfc283b362
TheKnaveArlecchino <bozhidara.n.aleksandrova@gmail.com>
2025-06-10 16:53:54 +0300

    Last edits of Kafka cache & generic cache

--- MovieStoreB/MovieStoreB.DL/Kafka/KafkaProducer.cs
+++ MovieStoreB/MovieStoreB.DL/Kafka/KafkaProducer.cs
@@ -1,15 +1,19 @@
 ﻿using Confluent.Kafka;
+using Microsoft.Extensions.Options;
+using MovieStoreB.Models.Configurations.CachePopulator;
 using MovieStoreB.Models.DTO;
 using MovieStoreB.Models.Serialization;
 
 namespace MovieStoreB.DL.Kafka
 {
-    internal class KafkaProducer<TKey, TData> : IKafkaProducer<TData> where TData : CacheItem<TKey> where TKey : notnull
+    internal class KafkaProducer<TKey, TData, TConfiguration> : IKafkaProducer<TData> where TData : ICacheItem<TKey> where TKey : notnull
+        where TConfiguration : CacheConfiguration
     {
         private readonly ProducerConfig _config;
         private readonly IProducer<TKey, TData> _producer;
+        private readonly IOptionsMonitor<TConfiguration> _kafkaConfig;
 
-        public KafkaProducer()
+        public KafkaProducer(IOptionsMonitor<TConfiguration> kafkaConfig)
         {
             _config = new ProducerConfig()
             {
@@ -24,11 +28,12 @@ namespace MovieStoreB.DL.Kafka
             _producer = new ProducerBuilder<TKey, TData>(_config)
                 .SetValueSerializer(new MsgPackSerializer<TData>())
                 .Build();
+            _kafkaConfig = kafkaConfig;
         }
 
         public async Task Produce(TData message)
         {
-            await _producer.ProduceAsync("test", new Message<TKey, TData>
+            await _producer.ProduceAsync(_kafkaConfig.CurrentValue.Topic, new Message<TKey, TData>
             {
                 Key = message.GetKey(),
                 Value = message
@@ -37,9 +42,11 @@ namespace MovieStoreB.DL.Kafka
 
         public async Task ProduceAll(IEnumerable<TData> messages)
         {
-            var tasks = messages.Select(message => Produce(message));
+            //var tasks = messages.Select(message => Produce(message));
 
-            await Task.WhenAll(tasks);
+            //await Task.WhenAll(tasks);
+
+            await ProduceBatches(messages);
         }
 
         public async Task ProduceBatches(IEnumerable<TData> messages)
--- MovieStoreB/MovieStoreB.Models/DTO/Actor.cs
+++ MovieStoreB/MovieStoreB.Models/DTO/Actor.cs
@@ -1,8 +1,24 @@
-﻿namespace MovieStoreB.Models.DTO
+﻿
+using MessagePack;
+
+namespace MovieStoreB.Models.DTO
 {
-    public record Actor(string Id, string Name) : CacheItem<string>
+    [MessagePackObject]
+    public class Actor : ICacheItem<string>
     {
-        public override string GetKey()
+        [Key(0)]
+        public string Id { get; set; } = string.Empty;
+
+        [Key(1)]
+        public DateTime DateInserted { get; set; }
+
+        [Key(2)]
+        public string Name { get; set; } = string.Empty;
+
+        [Key(3)]
+        public string Bio { get; set; } = string.Empty;
+
+        public string GetKey()
         {
             return Id;
         }
